40626596256c9fd2395165173ae47a0d03c99c37
Chase Hardin <dhardin@wrberkley.com>
2017-02-27 16:42:33 -0600

    Add Semantic UI dependencies.

--- MyMovieTheater.Web/README.md
+++ MyMovieTheater.Web/README.md
@@ -1,5 +1,7 @@
-# Angular QuickStart Source
+# Movie Theater
 [![Build Status][travis-badge]][travis-badge-url]
+
+// http://semantic-ui.com/
 
 This repository holds the TypeScript source code of the [angular.io quickstart](https://angular.io/docs/ts/latest/quickstart.html),
 the foundation for most of the documentation samples and potentially a good starting point for your application.
--- MyMovieTheater.Web/src/app/app.module.ts
+++ MyMovieTheater.Web/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {SuiModule} from 'ng2-semantic-ui/ng2-semantic-ui';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
+  imports:      [ BrowserModule ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
